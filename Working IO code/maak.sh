60cae7762acbece2ca1017267724e0539d0c825a
Anne Jan Brouwer <brouwer@annejan.com>
2020-01-15 17:48:43 +0100

    maak clean

--- Working IO code/maak.sh
+++ Working IO code/maak.sh
@@ -1,11 +1,17 @@
-#!/usr/local/env bash 
+#!/usr/bin/env bash 
 
-avr-gcc -ILib -Iavr/include -x c -funsigned-char -funsigned-bitfields -DDEBUG  -O1 -ffunction-sections -fdata-sections -fpack-struct -fshort-enums -mrelax -g2 -Wall -mmcu=attiny1617 -Bdev/attiny1617  -c -std=gnu99 -MD -MP -MF -mmcu=attiny1617 -o ISR_IO/main.o ISR_IO/main.c 
+if [ "$1" == "clean" ]; then
 
-avr-gcc -ILib -Iavr/include -x c -funsigned-char -funsigned-bitfields -DDEBUG  -O1 -ffunction-sections -fdata-sections -fpack-struct -fshort-enums -mrelax -g2 -Wall -mmcu=attiny1617 -Bdev/attiny1617  -c -std=gnu99 -MD -MP -MF -mmcu=attiny1617 -o Lib/I2C.o Lib/I2C.c 
+rm Lib/*.o ISR_IO/*.o Lib/*.d ISR_IO/*.d ISR_IO.elf 
 
-avr-gcc -ILib -Iavr/include -x c -funsigned-char -funsigned-bitfields -DDEBUG  -O1 -ffunction-sections -fdata-sections -fpack-struct -fshort-enums -mrelax -g2 -Wall -mmcu=attiny1617 -Bdev/attiny1617  -c -std=gnu99 -MD -MP -MF -mmcu=attiny1617 -o Lib/main_def.o Lib/main_def.c 
+else 
 
-avr-gcc -ILib -Iavr/include -x c -funsigned-char -funsigned-bitfields -DDEBUG  -O1 -ffunction-sections -fdata-sections -fpack-struct -fshort-enums -mrelax -g2 -Wall -Wa,-mgcc-isr -mmcu=attiny1617 -Bdev/attiny1617  -c -std=gnu99 -MD -MP -MF -mmcu=attiny1617 -o Lib/resources.o Lib/resources.c 
+avr-gcc -ILib -Iavr/include -x c -funsigned-char -funsigned-bitfields -DDEBUG -O1 -ffunction-sections -fdata-sections -fpack-struct -fshort-enums -mrelax -g2 -Wall -mmcu=attiny1617 -Bdev/attiny1617  -c -std=gnu99 -MD -MP -MF Lib/I2C.d -o Lib/I2C.o Lib/I2C.c 
 
+avr-gcc -ILib -Iavr/include -x c -funsigned-char -funsigned-bitfields -DDEBUG -O1 -ffunction-sections -fdata-sections -fpack-struct -fshort-enums -mrelax -g2 -Wall -mmcu=attiny1617 -Bdev/attiny1617  -c -std=gnu99 -MD -MP -MF Lib/main_def.d -o Lib/main_def.o Lib/main_def.c 
 
+avr-gcc -ILib -Iavr/include -x c -funsigned-char -funsigned-bitfields -DDEBUG -O1 -ffunction-sections -fdata-sections -fpack-struct -fshort-enums -mrelax -g2 -Wall -Wa,-mgcc-isr -mmcu=attiny1617 -Bdev/attiny1617  -c -std=gnu99 -MD -MP -MF Lib/resources.d -o Lib/resources.o Lib/resources.c 
+
+avr-gcc -ILib -Iavr/include -x c -funsigned-char -funsigned-bitfields -DDEBUG -O1 -ffunction-sections -fdata-sections -fpack-struct -fshort-enums -mrelax -g2 -Wall -mmcu=attiny1617 -Bdev/attiny1617  -c -std=gnu99 -MD -MP -MF ISR_IO/main.d -o ISR_IO/main.o ISR_IO/main.c 
+
+fi
