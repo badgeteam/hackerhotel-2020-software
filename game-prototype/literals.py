8cdb388c5c75d0a881f33d82d1b77ff228cb930f
Sake Blok <sake@euronet.nl>
2020-02-01 21:00:04 +0100

    Fixed merge conflict

--- game-prototype/literals.py
+++ game-prototype/literals.py
@@ -1,52 +1,52 @@
-literals = {'HELP':'Welcome to the "Escape from Hacker Hotel!!!" challenge.\n\
-\n\
-You are locked in the hotel, just like servants of the pharaohs\n\
-were locked in the Pyramids with their masters.\n\
-You will need to hack yourself a way out of the hotel before\n\
-the end of the weekend (when the hotel will be closed up\n\
-forever, WHOAHAHAHAHAHAHA)!\n\
-\n\
-h or ?      show this help\n\
-a           show the hieroglyph alphabet\n\
-w           who am i, your avatar\n\
-x           exit to the previous location\n\
-e <room>    enter the given room\n\
-l           look around the room\n\
-l <o>       look at object <o>\n\
-o <o>       open object <o>\n\
-p <i>       pickup item <i>\n\
-d <i>       drop item <i>\n\
-i           list items you are carrying\n\
-t <p>       talk to "person" <p>\n\
-g <i> <p>   give item <i> to person <p>\n\
-r <o>       read object <o>\n\
-u <o>       use object <o>\n\
-u <i> <o>   use item <i> on object <o>\n\
-\n\
-Every object has a single letter identifier enclosed in brackets. So \n\
-when you see "The [R]eception" in the game, you can use "e r" to enter\n\
-the Reception.\n\
-\n\
-[Please note that some output is in UTF-8. However, all user input\n\
+literals = {'HELP':'Welcome to the "Escape from Hacker Hotel!!!" challenge.\r\n\
+\r\n\
+You are locked in the hotel, just like servants of the pharaohs\r\n\
+were locked in the Pyramids with their masters.\r\n\
+You will need to hack yourself a way out of the hotel before\r\n\
+the end of the weekend (when the hotel will be closed up\r\n\
+forever, WHOAHAHAHAHAHAHA)!\r\n\
+\r\n\
+h or ?      show this help\r\n\
+a           show the hieroglyph alphabet\r\n\
+w           who am i, your avatar\r\n\
+x           exit to the previous location\r\n\
+e <room>    enter the given room\r\n\
+l           look around the room\r\n\
+l <o>       look at object <o>\r\n\
+o <o>       open object <o>\r\n\
+p <i>       pickup item <i>\r\n\
+d <i>       drop item <i>\r\n\
+i           list items you are carrying\r\n\
+t <p>       talk to "person" <p>\r\n\
+g <i> <p>   give item <i> to person <p>\r\n\
+r <o>       read object <o>\r\n\
+u <o>       use object <o>\r\n\
+u <i> <o>   use item <i> on object <o>\r\n\
+\r\n\
+Every object has a single letter identifier enclosed in brackets. So \r\n\
+when you see "The [R]eception" in the game, you can use "e r" to enter\r\n\
+the Reception.\r\n\
+\r\n\
+[Please note that some output is in UTF-8. However, all user input\r\n\
 should be entered in ASCII only]',
-'ALPHABET': 'A = 𓄿   B = 𓂾   C = 𓎡   D = 𓂧   E = 𓆄\n\
-\n\
-F = 𓆑   G = 𓎼   H = 𓉔   I = 𓆄   J = 𓆓\n\
-\n\
-K = 𓎡   L = 𓃭   M = 𓅓   N = 𓈖   O = 𓅱\n\
-\n\
-P = 𓊪   Q = 𓈎   R = 𓂋   S = 𓋴   T = 𓏏\n\
-\n\
-U = 𓅱   V = 𓆑   W = 𓅱   X = 𓋴   Y = 𓆄𓆄\n\
-\n\
+'ALPHABET': 'A = 𓄿   B = 𓂾   C = 𓎡   D = 𓂧   E = 𓆄\r\n\
+\r\n\
+F = 𓆑   G = 𓎼   H = 𓉔   I = 𓆄   J = 𓆓\r\n\
+\r\n\
+K = 𓎡   L = 𓃭   M = 𓅓   N = 𓈖   O = 𓅱\r\n\
+\r\n\
+P = 𓊪   Q = 𓈎   R = 𓂋   S = 𓋴   T = 𓏏\r\n\
+\r\n\
+U = 𓅱   V = 𓆑   W = 𓅱   X = 𓋴   Y = 𓆄𓆄\r\n\
+\r\n\
 Z = 𓊃 ',
-'LOCATION':'\nLocation: ',
+'LOCATION':'Location: ',
 'SPACE':' ',
-'LF':'\n\n\n\n',
+'LF':'\r\n\r\n\r\n\r\n',
 'COMMA':', ',
 'PROMPT':'? ',
 'QUIT':'Thank you for staying, you can check out any time you want, but you may never leave!',
-'LOOK':'\nI see the following: ',
+'LOOK':'I see the following: ',
 'DONTSEE':'I don\'t see that!',
 'CANTLOOK':'You can\'t look inside ',
 'CANTENTER':'You can\'t enter that location.',
@@ -61,7 +61,7 @@ Z = 𓊃 ',
 'CANTUSEITEM':'You can\'t use this item on this object.',
 'RESPONSE':'(your response) ? ',
 'INCORRECT':'That is incorrect!',
-'CARRYTWO':'You can only carry 2 objects, please drop another object\nif you really need this object.',
+'CARRYTWO':'You can only carry 2 objects, please drop another object\r\nif you really need this object.',
 'ALREADYCARRYING':'You are already carrying that object.',
 'NOWCARRING':'You are now carrying: ',
 'EMPTYHANDS':'Uhmmm... you\'re not carrying anything!',
@@ -69,7 +69,7 @@ Z = 𓊃 ',
 'RETURNING':'A hotel clerk brought the item back to its original location.',
 'NOTCARRYING':'You are not carrying that object.',
 'IDDQDMSG':'IDDQD (for testing purposes only, remove from final code!!!)',
-'CONGRATS':'CONGRATULATIONS!!!\nThe spell has been broken and you found your way out of the hotel.',
+'CONGRATS':'CONGRATULATIONS!!!\r\nThe spell has been broken and you found your way out of the hotel.',
 'CANTGIVE':'You can\'t give this item to this person.',
 'HELLO':'Hello, my name is ',
 'ANUBIS':'Anubis',
@@ -79,7 +79,7 @@ Z = 𓊃 ',
 'PLEASED':', I\'m pleased to meet you!',
 'NOTPOSSIBLE':'Sorry, that is not possible',
 'ERROR':'Hmm... something is wrong, please contact someone from badge.team!',
-'PRIEST':'I\'m sorry my child, I\'m a little blind. Can you\nplease tell me how many times you kneeled and\nwhich element you will bring to the Pyramid?\n\n[entry format: <number> <element>]',
+'PRIEST':'I\'m sorry my child, I\'m a little blind. Can you\r\nplease tell me how many times you kneeled and\r\nwhich element you will bring to the Pyramid?\r\n\r\n[entry format: <number> <element>]',
 'PLEASEOFFER':'Please offer something from your inventory.',
 'BADOFFERING':'You call that an offering, you should be ashamed of yourself!',
 'YOURPART':'Your part of the access code is ',
