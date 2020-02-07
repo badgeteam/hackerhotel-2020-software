bf6e2a305cfa404a6d5065d8696184b3f437b504
Anne Jan Brouwer <brouwer@annejan.com>
2020-02-07 14:24:23 +0100

    Magical mystery cleanup, look mom, no divisions

--- Working IO code/Lib/simon.c
+++ Working IO code/Lib/simon.c
@@ -25,8 +25,10 @@ uint8_t simonState[BASTET_LENGTH] = {0};
 uint8_t simonPos = 0,
         simonInputPos = 0,
         simonTimer = 0,
-        simonWait = 0;
-uint8_t simonGameState = BASTET_BOOT;
+        simonWait = 0,
+        simonCounter = 0;
+uint8_t simonGameState = BASTET_BOOT,
+        simonNextGameState = BASTET_GAME_INPUT;
 
 /**
  * Main game loop
@@ -46,40 +48,47 @@ uint8_t BastetDictates() {
         for (uint8_t i = 0; i < BASTET_LENGTH; i++) {
             simonState[i] = (lfsr() % 4);
         }
-        simonPos = 1;
         simonGameState = BASTET_GAME_START;
     }
 
     if (BASTET_GAME_START == simonGameState && BASTET == gameNow) {
         gameNow = BASTET;
         simonGameState = BASTET_GAME_INTRO;
-        simonPos = 1;
+        simonPos = 0;
         simonInputPos = 0;
         simonTimer = 0;
+        simonCounter = 0;
     }
 
     if (BASTET_GAME_INTRO == simonGameState) {
-        uint8_t pos = simonTimer / (3 - (simonPos>>1));
-        if (pos > 6) {
+        if (simonTimer > 5) {   // ±.33 seconds
+            simonCounter++;
+            simonTimer = 0;
+        }
+        if (simonCounter > 6) {
             simonGameState = BASTET_GAME_SHOW_PATTERN;
 //            for (uint8_t n=0; n<6; n++){
 //                iLED[HCKR[R][n]] = 1;
 //            }
             simonTimer = 0;
+            simonCounter = 0;
             return 0;
         }
-        iLED[HCKR[R][pos]] = dimValue;
+        iLED[HCKR[R][simonCounter]] = dimValue;
     }
 
     if (BASTET_GAME_SHOW_PATTERN == simonGameState) {
-        // assuming 15Hz
-        uint8_t pos = simonTimer / (15 - (simonPos>>1));    // is that even fair?
-        if (pos > simonPos) {
+        if (simonTimer > 15) {   // ± ! second
+            simonCounter++;
+            simonTimer = 0;
+        }
+        if (simonCounter > simonPos) {
             simonGameState = BASTET_GAME_INPUT;
             simonLed(0);
+            simonTimer = 0;
             return 0;
         }
-        simonLed(simonState[pos]);
+        simonLed(simonState[simonCounter]);
     }
 
     if (BASTET_GAME_INPUT == simonGameState) {
@@ -108,29 +117,35 @@ uint8_t BastetDictates() {
                 simonTimer = 0;
                 simonLed(choice);
                 simonGameState = BASTET_GAME_WAIT_LEDS;
+                simonNextGameState = BASTET_GAME_INPUT;
 
                 // is the below check correct .. do I have off by one ?
-                if (simonState[simonInputPos+1] == choice) {
-                    // TODO correct, next sound (textadv?)
+                if (simonState[simonInputPos] == choice) {
                     simonInputPos++;
-                    if (simonInputPos == simonPos) {
+                    if (simonInputPos >= simonPos) {
+                        effect = 0x42;
                         simonPos++;
+                        simonTimer = 0;
+                        simonCounter = 0;
+                        simonNextGameState = BASTET_GAME_SHOW_PATTERN;
                     }
                 } else {
                     effect = 0x0105;    // TODO fail sound
-                    simonTimer = 0;
-                    // perhaps again some nextState magic to show incorrect selection
                     for (uint8_t n=0; n<6; n++){
                         iLED[HCKR[R][n]] = dimValue;
                     }
-                    simonGameState = BASTET_GAME_OUTRO;
+                    simonTimer = 0;
+                    simonCounter = 0;
+                    simonNextGameState = BASTET_GAME_OUTRO;
                 }
 
-                if (simonInputPos == BASTET_LENGTH || simonPos > BASTET_LENGTH) { // beetje dubbel
+                if (simonInputPos >= BASTET_LENGTH || simonPos >= BASTET_LENGTH) { // beetje dubbel
                     // TODO win animu !!
-                    effect = 0x0106;    // TODO win sound
+                    effect = 0x0106;    // TODO win sound ?!
                     UpdateState(BASTET_COMPLETED);
-                    simonGameState = BASTET_GAME_OVER;
+                    simonTimer = 0;
+                    simonCounter = 0;
+                    simonNextGameState = BASTET_GAME_OVER;
                 }
             }
         
@@ -145,17 +160,24 @@ uint8_t BastetDictates() {
             // on to next state after ±.5 second
             simonLed(0);  // LEDs off
             simonWait = 0;
-            simonGameState = BASTET_GAME_INPUT;
+            simonTimer = 0;
+            simonCounter = 0;
+            simonGameState = simonNextGameState;
         }
     }
 
     if (BASTET_GAME_OUTRO == simonGameState) {
-        uint8_t pos = simonTimer / (3 - (simonPos>>1));
-        if (pos > 6) {
+        if (simonTimer > 5) {   // ±.33 second
+            simonCounter++;
+            simonTimer = 0;
+        }
+        if (simonCounter > 6) {
+            simonTimer = 0;
+            simonCounter = 0;
             simonGameState = BASTET_GAME_OVER;
             return 0;
         }
-        iLED[HCKR[R][5-pos]] = 1;
+        iLED[HCKR[R][5-simonCounter]] = 1;
     }
 
     if (BASTET_GAME_OVER == simonGameState) {
@@ -184,19 +206,19 @@ void simonLed(uint8_t val) {
         iLED[WING[L][0]] = dimValue;
         iLED[WING[L][1]] = dimValue;
         iLED[WING[L][2]] = dimValue;
-        effect = 0x0101;
+        effect = 0x013f;
     } else if (val == 2) {
         iLED[WING[L][3]] = dimValue;
         iLED[WING[L][4]] = dimValue;
-        effect = 0x0102;
+        effect = 0x015f;
     } else if (val == 3) {
         iLED[WING[R][0]] = dimValue;
         iLED[WING[R][1]] = dimValue;
         iLED[WING[R][2]] = dimValue;
-        effect = 0x0103;
+        effect = 0x017f;
     } else if (val == 4) {
         iLED[WING[R][3]] = dimValue;
         iLED[WING[R][4]] = dimValue;
-        effect = 0x0104;
+        effect = 0x019f;
     }
 }
